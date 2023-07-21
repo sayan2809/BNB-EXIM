20005364932beee09180097e60420446370db362
Anuran12 <anuranarts@gmail.com>
2023-07-22 02:02:11 +0530

    product filter

--- Products/products.js
+++ Products/products.js
@@ -7,15 +7,16 @@ http.onload = function(){
       let output = "";
       for(let item of products){
          output += `
-            <div class="product">
+            <div class="product ${item.category}">
                <img src="${item.image}" alt="${item.description}">
                <p class="title">${item.title}</p>
+               <p class="category">${item.category}</p>
                <p class="description">${item.description}</p>
                <p class="price">
                   <span>${item.price}</span>
                   <span>$</span>
                </p>
-               <p class="cart">Order Now<i class="bx bx-cart-alt"></i></p>
+               <a href="https://wa.me/919804793204/?text=Hi%20there,%20I%20would%20like%20to%20buy%20%0A%0A*${item.title}*%0A%0A_${item.category}_%0A%0A&#96;&#96;&#96;${item.description}&#96;&#96;&#96;%0A%0A*$${item.price}*" class="cart">Order Now<i class="bx bx-cart-alt"></i></a>
             </div>
          `;
       }
