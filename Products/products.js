50ab90d429daaf147ced8ebd27f778da832deb17
Anuran12 <anuranarts@gmail.com>
2023-07-23 14:00:03 +0530

    quantity added

--- Products/products.js
+++ Products/products.js
@@ -12,11 +12,12 @@ http.onload = function(){
                <p class="title">${item.title}</p>
                <p class="category">${item.category}</p>
                <p class="description">${item.description}</p>
+               <p class="quantity">${item.quantity}</p>
                <p class="price">
                   <span>${item.price}</span>
                   <span>$</span>
                </p>
-               <a href="https://wa.me/919804793204/?text=Hi%20there,%20I%20would%20like%20to%20buy%20%0A%0A*${item.title}*%0A%0A_${item.category}_%0A%0A&#96;&#96;&#96;${item.description}&#96;&#96;&#96;%0A%0A*$${item.price}*" class="cart">Order Now<i class="bx bx-cart-alt"></i></a>
+               <a href="https://wa.me/919804972432/?text=Hi%20there,%20I%20would%20like%20to%20buy%20%0A%0A*${item.title}*%0A%0A_${item.category}_%0A%0A&#96;&#96;&#96;${item.description}&#96;&#96;&#96;%0A%0A*$${item.price}*" class="cart">Order Now<i class="bx bx-cart-alt"></i></a>
             </div>
          `;
       }
