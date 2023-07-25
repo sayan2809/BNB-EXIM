dd8d60bf815a816c63d4f68bbbfed65a7b532aeb
sayan2809 <dassayan966@gmail.com>
2023-07-25 10:45:00 +0530

    fix

--- script.js
+++ script.js
@@ -150,18 +150,15 @@ var accordion = (function () {
 
 /*FAQs end*/
 
-
-
-
 let http = new XMLHttpRequest();
-http.open('get', 'feature.json', true);
+http.open("get", "feature.json", true);
 http.send();
-http.onload = function(){
-   if(this.readyState == 4 && this.status == 200){
-      let products = JSON.parse(this.responseText);
-      let output = "";
-      for(let item of products){
-         output += `
+http.onload = function () {
+  if (this.readyState == 4 && this.status == 200) {
+    let products = JSON.parse(this.responseText);
+    let output = "";
+    for (let item of products) {
+      output += `
             <div class="product ${item.category}">
                <img src="${item.image}" alt="${item.description}">
                <p class="title">${item.title}</p>
@@ -174,7 +171,7 @@ http.onload = function(){
                <a href="https://wa.me/919804972432/?text=Hi%20there,%20I%20would%20like%20to%20buy%20%0A%0A*${item.title}*%0A%0A&#96;&#96;&#96;${item.description}&#96;&#96;&#96;%0A%0A${item.quantity}" class="cart">Order Now<i class="bx bx-cart-alt"></i></a>
             </div>
          `;
-      }
-      document.querySelector(".products").innerHTML = output;
-   }
-}
+    }
+    document.querySelector(".products").innerHTML = output;
+  }
+};
